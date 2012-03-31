a4316c52baeb1e66e57d93b78ea7e9038f4b1cc1
Ryan <ryanto@gmail.com>
2012-03-31 16:28:21 -0400

    dont need to define method name inside of the eval

--- lib/rails-view-helper-objects/define_helper_method.rb
+++ lib/rails-view-helper-objects/define_helper_method.rb
@@ -2,10 +2,10 @@ module RailsViewHelperObjects::DefineHelperMethod
 
   def self.included klass
 
+    method_name = klass.name.underscore.split("/").last
+    
     # create a view helper that returns klass.new
     ActionView::Helpers.module_eval do
-      method_name = klass.name.underscore.split("/").last
-
       define_method method_name do |*args|
         obj = klass.new(*args)
         # give the object access to the view
